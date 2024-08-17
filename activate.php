51676cf019b296204e9c7006d57018705c00fb69
Christopher Pipkin <Christopher.Pipkin@insight.com>
2024-08-17 12:06:28 -0400

    .

--- activate.php
+++ activate.php
@@ -1,6 +1,7 @@
 <?php
 function create_tables() {
     global $wpdb;
+    $charset_collate = $wpdb->get_charset_collate();
 
     $table1 = "CREATE TABLE badgerMCT_cultures (
         shop_lot_num INT(6) AUTO_INCREMENT,
@@ -8,7 +9,8 @@ function create_tables() {
         received_date DATE,
         vendor CHAR(50),
         ven_lot_num CHAR(20),
-        PRIMARY KEY(shop_lot_num))";
+        PRIMARY KEY(shop_lot_num)
+        ) $charset_collate;";
     
     require_once ( ABSPATH . 'wp-admin/includes/upgrade.php');
     dbDelta( $table1 );
--- badgerMCT.php
+++ badgerMCT.php
@@ -8,6 +8,11 @@
 * License: GPLv3
 * License URI: https://www.gnu.org/licenses/gpl-3.0.html
 */
+
+if ( ! defined( 'ABSPATH') ) {
+    exit;
+}
+
 include 'activate.php';
 include 'menu_conf.php';
 ?>
