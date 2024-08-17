78e8091c2682c3fab6392027b720ea614cc47725
Christopher Pipkin <Christopher.Pipkin@insight.com>
2024-08-16 22:12:50 -0400

    .

--- badgerMCT.php
+++ badgerMCT.php
@@ -12,18 +12,31 @@
 function badger_main() {
     ?>
     <div class="wrap">
-      <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
-      <form action="options.php" method="post">
-        <?php
-        // output security fields for the registered setting "wporg_options"
-        settings_fields( 'wporg_options' );
-        // output setting sections and their fields
-        // (sections are registered for "wporg", each field is registered to a specific section)
-        do_settings_sections( 'wporg' );
-        // output save settings button
-        submit_button( __( 'Save Settings', 'textdomain' ) );
-        ?>
-      </form>
+        <h1>
+            <?php echo esc_html( get_admin_page_title() ); ?>
+        </h1>
+        <form action="options.php" method="post">
+            <?php
+                // output security fields for the registered setting "wporg_options"
+                settings_fields( 'wporg_options' );
+                // output setting sections and their fields
+                // (sections are registered for "wporg", each field is registered to a specific section)
+                do_settings_sections( 'wporg' );
+                // output save settings button
+                submit_button( __( 'Save Settings', 'textdomain' ) );
+            ?>
+        </form>
+    </div>
+    <?php
+}
+
+function badgermct_cultures_page() {
+    ?>
+    <div class="wrap">
+        <h1>
+            <?php echo esc_html( get_admin_page_titiel() ); ?>
+        </h1>
+        <p>This is just place holder text</p>
     </div>
     <?php
 }
