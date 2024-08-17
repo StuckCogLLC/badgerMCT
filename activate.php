5ce0018f35ae40e4d457a00754fe0681370cc909
Christopher Pipkin <Christopher.Pipkin@insight.com>
2024-08-17 12:52:11 -0400

    .

--- activate.php
+++ activate.php
@@ -4,7 +4,7 @@ function create_tables() {
     global $wpdb;
     $charset_collate = $wpdb->get_charset_collate();
 
-    $table1 = "CREATE TABLE badgerMCT_cultures (
+    $table1 = "CREATE TABLE IF NOT EXISTS badgerMCT_cultures (
         shop_lot_num INT(6) AUTO_INCREMENT,
         mush_type CHAR(50),
         received_date DATE,
@@ -13,6 +13,7 @@ function create_tables() {
         PRIMARY KEY(shop_lot_num)
         ) $charset_collate;";
     
-    $wpdb->query( "$table" );
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta( $table1 );
 }
 ?>
