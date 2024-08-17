4c8a376c20ad5720957786ce0d2ffc99d9ceac65
Christopher Pipkin <Christopher.Pipkin@insight.com>
2024-08-16 20:05:32 -0400

    init

---
+++ badgerMCT.php
@@ -0,0 +1,9 @@
+/*
+* Plugin Name: badgerMCT
+* Description: mushroom cultivation tracker
+* Version: 0.1
+* Author: Stuck Cog LLC
+* Author URI: https://www.stuckcogllc.com
+* License: GPLv3
+* License URI: https://www.gnu.org/licenses/gpl-3.0.html
+*/
