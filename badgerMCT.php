8b183fdb32509b086ad1e6d0e6a4949ae2be95ce
Christopher Pipkin <Christopher.Pipkin@insight.com>
2024-09-17 19:08:57 -0400

    .

--- badgerMCT.php
+++ badgerMCT.php
@@ -15,6 +15,24 @@ if ( ! defined( 'ABSPATH') ) {
     exit;
 }
 
-include 'activate.php';
+function create_tables() {
+    global $wpdb;
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $table1 = "CREATE TABLE IF NOT EXISTS badgerMCT_cultures (
+        shop_lot_num INT(6) AUTO_INCREMENT,
+        mush_type CHAR(50),
+        received_date DATE,
+        vendor CHAR(50),
+        ven_lot_num CHAR(20),
+        PRIMARY KEY(shop_lot_num)
+        ) $charset_collate;";
+    
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta( $table1 );
+}
+register_activation_hook( __FILE__, 'create_tables' );
+
+#include 'activate.php';
 include 'menu_conf.php';
 ?>
