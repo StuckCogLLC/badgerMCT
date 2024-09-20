4e4681f2716ec15c440f89718b40f56e2ae7d6bf
markpipkin <mark.pipkin@stuckcog.com>
2024-09-20 17:17:36 -0400

    .

--- menu_conf.php
+++ menu_conf.php
@@ -29,20 +29,7 @@ function badgermct_cultures() {
 function badgermct_dashboard_page() {
     ?>
     <div class="wrap">
-        <h1>
-            <?php echo esc_html( get_admin_page_title() ); ?>
-        </h1>
-        <form action="options.php" method="post">
-            <?php
-                // output security fields for the registered setting "wporg_options"
-                settings_fields( 'wporg_options' );
-                // output setting sections and their fields
-                // (sections are registered for "wporg", each field is registered to a specific section)
-                do_settings_sections( 'wporg' );
-                // output save settings button
-                submit_button( __( 'Save Settings', 'textdomain' ) );
-            ?>
-        </form>
+        <p>Place holder for Dashboard</p>
     </div>
     <?php
 }
