36ed2beeb7bfce49221094035cfd2a156f202b92
Christopher Pipkin <Christopher.Pipkin@insight.com>
2024-09-27 19:47:49 -0400

    .

--- manage_cultures.php
+++ manage_cultures.php
@@ -3,17 +3,11 @@
             <?php echo esc_html( get_admin_page_title() ); ?>
         </h1>
         <form action="<?php menu_page_url( 'badgermct_cultures_page' ) ?>" method="POST">
-            Your name: <input type="text" name="reviewer_name"><br><br>
-            How many stars would you give us? 
-                <select name="star_rating">
-                    <option value="1">1 star</option>
-                    <option value="2">2 stars</option>
-                    <option value="3">3 stars</option>
-                    <option value="4">4 stars</option>
-                    <option value="5">5 stars</option>
-                </select><br><br>
-            Your review: <br>
-                <textarea name="details" rows="10" cols="30"></textarea><br><br>
+            Shop Lot Number: <input type="int" name="shop_lot_num"><br><br>
+            Mushroom Strain: <input type="text" name="mush_type"><br><br>
+            Date Received: <input type="date" name="received_date"><br><br>
+            Vendor: <input type="text" name="vendor"><br><br>
+            Vendor Lot Number: <input type="text" name="ven_lot_num"><br><br>
             <input type="submit" value="Submit" name="submit">
     </form>
     </div>
