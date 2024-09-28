71b10598b67373a9a99f805929863597326d7683
Christopher Pipkin <Christopher.Pipkin@insight.com>
2024-09-27 20:21:46 -0400

    .

--- manage_cultures.php
+++ manage_cultures.php
@@ -1,30 +1,54 @@
 <div class="wrap">
-        <h1>
-            <?php echo esc_html( get_admin_page_title() ); ?>
-        </h1>
+    <h1>
+        <?php echo esc_html( get_admin_page_title() ); ?>
+    </h1>
+    <form action="" method="post">
         <table class="form-table">
             <tbody>
                 <tr>
                     <th>
-                    Shop Lot Number:
+                        Shop Lot Number:
                     </th>
                     <th>
                         <input type="int" name="shop_lot_num">
                     </th>
                 </tr>
-
-
+                <tr>
+                    <th>
+                        Mushroom Strain:
+                    </th>
+                    <th>
+                        <input type="text" name="mush_type">
+                    </th>
+                </tr>
+                <tr>
+                    <th>
+                        Date Received:
+                    </th>
+                    <th>
+                        <input type="date" name="received_date">
+                    </th>
+                </tr>
+                <tr>
+                    <th>
+                        Vendor:
+                    </th>
+                    <th>
+                        <input type="text" name="vendor">
+                    </th>
+                </tr>
+                <tr>
+                    <th>
+                        Vendor Lot Number:
+                    </th>
+                    <th>
+                        <input type="text" name="ven_lot_num">
+                    </th>
+                </tr>
+                <tr>
+                    <input type="submit" value="Add" name="add">
+                </tr>
             </tbody>
         </table>
-            <div class="postbox">
-                <form action="<?php menu_page_url( 'badgermct_cultures_page' ) ?>" method="POST">
-                     <br><br>
-                    Mushroom Strain: <input type="text" name="mush_type"><br><br>
-                    Date Received: <input type="date" name="received_date"><br><br>
-                    Vendor: <input type="text" name="vendor"><br><br>
-                    Vendor Lot Number: <input type="text" name="ven_lot_num"><br><br>
-                    <input type="submit" value="Add" name="add">
-                </form>
-            <div>
-        </div>
-    </div>
+    </form>
+</div>
