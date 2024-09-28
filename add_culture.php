d8f0c4d24bee0720077caa495890c4afb28b19e8
Christopher Pipkin <Christopher.Pipkin@insight.com>
2024-09-27 21:55:53 -0400

    .

--- add_culture.php
+++ add_culture.php
@@ -1,28 +1,20 @@
 <?php
 if(isset($_POST['submit'])){
-    if($_POST['mush_type'] != ''&& $_POST['received_date'] != ''){
-        $wpdb->insert(
-            'badgerMCT_cultures',
-            array(
-                'mush_type' => $_POST['mush_type'],
-                'received_date' => $_POST['received_date'],
-                'vendor' => $_POST['vendor'],
-                'ven_lot_num' => $_POST['ven_lot_num'],
-            ),
-            array(
-                '%s',
-                '%s',
-                '%s',
-                '%s',
-            )
-        );
-        header("Location:https://www.stuckcogllc.com/wp-admin/admin.php?page=badgermct_cultures");
-    }
-} else {
-    ?>
-    <div class="wrap">
-        <p>Fill out the fields</p>
-    </div>
-    <?php
+    $wpdb->insert(
+        'badgerMCT_cultures',
+        array(
+            'mush_type' => $_POST['mush_type'],
+            'received_date' => $_POST['received_date'],
+            'vendor' => $_POST['vendor'],
+            'ven_lot_num' => $_POST['ven_lot_num'],
+        ),
+        array(
+            '%s',
+            '%s',
+            '%s',
+            '%s',
+        )
+    );
+    header("Location:https://www.stuckcogllc.com/wp-admin/admin.php?page=badgermct_cultures");
 }
 ?>
