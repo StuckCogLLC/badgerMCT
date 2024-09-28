8660ab360689227c95a87fa1071154f141dd12c7
Christopher Pipkin <Christopher.Pipkin@insight.com>
2024-09-27 22:59:21 -0400

    .

--- add_culture.php
+++ add_culture.php
@@ -8,14 +8,16 @@ if(isset($_POST['add'])){
     echo $_POST['vendor'];
     echo $_POST['ven_lot_num'];
 
+    $data = array(
+        'mush_type' => 'somemush',
+        'received_date' => '10/10/1010',
+        'vendor' => 'MycoLabs',
+        'ven_lot_num' => 'ml-123123123',
+    );
+
     $wpdb->insert(
         'badgerMCT_cultures',
-        array(
-            'mush_type' => 'somemush',
-            'received_date' => '10/10/1010',
-            'vendor' => 'MycoLabs',
-            'ven_lot_num' => 'ml-123123123',
-        ),
+        $data
     );
     #header("Location:https://www.stuckcogllc.com/wp-admin/admin.php?page=badgermct_cultures");
 }
