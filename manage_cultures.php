58dc5be6da86777cf620eadd9f8ba2c78b4ec9f5
Christopher Pipkin <Christopher.Pipkin@insight.com>
2024-09-27 20:13:33 -0400

    .

--- manage_cultures.php
+++ manage_cultures.php
@@ -2,14 +2,23 @@
         <h1>
             <?php echo esc_html( get_admin_page_title() ); ?>
         </h1>
-        <div width="25%">
-            <div title="Add Culture">
-                    <h2>Add Culture</h2>
-                    <br>
-            </div>
+        <table class="form-table">
+            <tbody>
+                <tr>
+                    <th>
+                    Shop Lot Number:
+                    </th>
+                    <th>
+                        <input type="int" name="shop_lot_num">
+                    </th>
+                </tr>
+
+
+            </tbody>
+        </table>
             <div class="postbox">
                 <form action="<?php menu_page_url( 'badgermct_cultures_page' ) ?>" method="POST">
-                    Shop Lot Number: <input type="int" name="shop_lot_num"><br><br>
+                     <br><br>
                     Mushroom Strain: <input type="text" name="mush_type"><br><br>
                     Date Received: <input type="date" name="received_date"><br><br>
                     Vendor: <input type="text" name="vendor"><br><br>
