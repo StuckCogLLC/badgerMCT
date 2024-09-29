0ee791db586f9d2d7194edd39719080fb2a0b0da
Christopher Pipkin <Christopher.Pipkin@insight.com>
2024-09-28 20:02:29 -0400

    .

--- manage_cultures.php
+++ manage_cultures.php
@@ -89,7 +89,7 @@
 <?php
 
     
-    echo $wpdb->query('SELECT vendor FROM wp_posts');
+    echo $wpdb->query('SELECT * FROM `badgerMCT_cultures`');
 
 /*    $query_culture_results = query_cultures();
     if ( $query_culture_results->num_rows > 0 ) {
