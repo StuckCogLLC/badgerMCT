32193fe4c2e293a2f561206e43870c8a6dc790b3
Christopher Pipkin <Christopher.Pipkin@insight.com>
2024-09-28 22:52:32 -0400

    .

--- manage_cultures.php
+++ manage_cultures.php
@@ -99,7 +99,7 @@ global $wpdb;
     echo query_cultures();
     $cultures = query_cultures();
     echo $cultures;
-    foreach (query_cultures() as $row) {
+    foreach ($cultures as $row) {
         echo "Lot Number: " . $row["shop_lot_num"] . " - Type: " . $row["mush_type"] . " - Date: " . $row["received_date"] . " - Vendor: " . $row["vendor"] . " - Vendor Lot: " . $row["ven_lot_num"] . "<br>";
     }
 
