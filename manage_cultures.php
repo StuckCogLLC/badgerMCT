2d47f1c69ae3a1a0d2c831c9db50a1fdc6159c02
Christopher Pipkin <Christopher.Pipkin@insight.com>
2024-09-28 22:09:19 -0400

    .

--- manage_cultures.php
+++ manage_cultures.php
@@ -87,8 +87,10 @@ global $wpdb;
     $wpdb->show_errors();
     $sql_query = $wpdb->prepare( "SELECT * FROM badgerMCT_cultures" );
     echo "----------<br>";
-    echo "Just the query: " . $wpdb->get_results($sql_query, ARRAY_A) . "<br>";
-
+    //echo "Just the query: " . $wpdb->get_results($sql_query, ARRAY_A) . "<br>";
+    //$somevar = $wpdb->get_results($sql_query, ARRAY_A) . "<br>";
+    //echo "query to a var: " . gettype($somevar) . "<br>";
+    //echo "function return: " . query_cultures() . "<br>";
     echo "----------<br>";
     $wpdb->print_error();
 
