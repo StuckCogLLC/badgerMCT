4cb1d046260e26b65342da52689a4d8fb8218aae
Christopher Pipkin <Christopher.Pipkin@insight.com>
2024-09-29 21:45:07 -0400

    .

--- menu_conf.php
+++ menu_conf.php
@@ -66,7 +66,7 @@ function badgermct_harvest_page() {
 }
 
 // add menu
-//( 'admin_menu', 'badgermct_dashboard' );
+add_action( 'admin_menu', 'badgermct_dashboard' );
 //add_action( 'admin_menu', 'badgermct_cultures' );
 //add_action( 'admin_menu', 'badgermct_grain' );
 //add_action( 'admin_menu', 'badgermct_harvest' );
