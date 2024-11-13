7361b309c161c4362d38d7fd5f719cf1f29eea05
C M Pipkin <droidclone@gmail.com>
2024-11-12 20:28:09 -0500

    Update manage_culture.php
    .

--- manage_culture.php
+++ manage_culture.php
@@ -35,7 +35,7 @@
     <h1>
         <?php echo esc_html( get_admin_page_title() ); ?>
     </h1>
-<!-- Start Add Culture form --> <!--
+<!-- Start Add Culture form -->
 <form action="https://www.stuckcogllc.com/wp-admin/admin.php?page=badgermct_culture" method="post">
         <h2>Add a culture</h2>
         <table style="width:50%;text-align:left">
@@ -75,7 +75,7 @@
             </tbody>
         </table>
         <input type="submit" value="Add" name="culture_insert">
-    </form> -->
+    </form>
 </div>
 <!-- list cultures -->
 <!--
