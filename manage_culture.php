9a0ffc5ca481fe49c481e327294e38cd17d94c42
C M Pipkin <droidclone@gmail.com>
2024-11-12 21:28:06 -0500

    Update manage_culture.php
    clean up

--- manage_culture.php
+++ manage_culture.php
@@ -1,11 +1,4 @@
 <?php
-    
-    // php functions
-    //function insert_culture($table, $data) {
-    //    $wpdb->flush();
-    //   global $wpdb;
-    //    $wpdb->insert($table, $data);
-    //}
     global $wpdb;
     $table = $wpdb->prefix . 'badgerMCT_cultures';
 
@@ -18,9 +11,7 @@
 
     // insert data into db if present
     if(isset($_POST['culture_insert'])){
-        global $wpdb;
-        
-
+        // table data
         $data = [
             "cult_num" => NULL,
             "mush_type" => isset($_POST['mush_type']),
@@ -29,9 +20,9 @@
             "vendor" => isset($_POST['vendor']),
             "ven_lot_num" => isset($_POST['ven_lot_num'])
         ];
-
+        // format
         $format = [NULL,'%s','%s','%s','%s','%s'];
-
+        // wpdb insert
         $wpdb->insert($table, $data, $format);
     }
 ?>
@@ -40,18 +31,7 @@
     <h1>
         <?php echo esc_html( get_admin_page_title() ); ?>
     </h1>
-<!-- debug -->
- <div>
-    <?php
-      echo($_POST['mush_type']);
-      echo($_POST['cult_type']);
-      echo($_POST['received_date']);
-      echo($_POST['vendor']);
-      echo($_POST['ven_lot_num']);
-    ?>
- </div>
-
-    <!-- Start Add Culture form -->
+<!-- Start Add Culture form -->
 <form action="https://www.stuckcogllc.com/wp-admin/admin.php?page=badgermct_culture" method="post">
         <h2>Add a culture</h2>
         <table style="width:50%;text-align:left">
@@ -106,14 +86,27 @@
 <div>
     <table>
         <tbody>
+            <tr>
+                <th style=width:10%>
+                    ID
+                </th>
+                <th style=width:10%>
+                    Type
+                </th>
+                <th style=width:10%>
+                    Mushroom
+                </th>
+                <th style=width:10%>
+                    Date
+                </th>
+                <th style=width:10%>
+                    Vendor
+                </th>
+                <th style=width:10%>
+                    Vendor Lot
+                </th>
+            </tr>
             <?php
-                echo "<tr><th style=\"width:10%\">" . "ID" .
-                    "</th><th style=\"width:10%\">" . "Type" .
-                    "</th><th style=\"width:10%\">" . "Mushroom" . 
-                    "</th><th style=\"width:10%\">" . "Date" . 
-                    "</th><th style=\"width:10%\">" . "Vendor" . 
-                    "</th><th style=\"width:10%\">" . "Vendor Lot" . 
-                    "</th></tr>";
                 $cultures = query_cultures($table);
                 foreach ($cultures as $culture) {
                     echo "<tr><th>" . $culture['cult_num'] . 
