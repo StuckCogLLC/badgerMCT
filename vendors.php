c58dafc57781105f9dafb35d2fc5359c42dba5da
C M Pipkin <droidclone@gmail.com>
2024-11-28 08:42:46 -0500

    added posting data to vendors table

--- vendors.php
+++ vendors.php
@@ -1,3 +1,21 @@
 <?php 
     echo "vendor page";
+
+    global $wpdb;
+    $table = $wpdb->prefix . 'badgerMCT_vendors';
+
+    // insert data into db if present
+    if(isset($_POST['vendor_insert'])){
+        // table data
+        $data = [
+            "ven_num" => NULL,
+            "ven_name" => $_POST['ven_name'],
+            "ven_phone" => $_POST['ven_phone'],
+            "ven_notes" => $_POST['ven_notes']
+        ];
+        // format
+        $format = [NULL,'%s','%s','%s'];
+        // wpdb insert
+        $wpdb->insert($table, $data);
+    }
 ?>
