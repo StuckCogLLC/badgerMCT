48a2d67f88e3c940275813bba0ce5eb2339e6d21
C M Pipkin <droidclone@gmail.com>
2024-11-28 09:08:17 -0500

    working on table display

--- vendors.php
+++ vendors.php
@@ -84,7 +84,8 @@
             <?php
                 $vendors = query_vendors($table);
                 foreach ($vendors as $vendor) {
-                    echo "<tr><th>" . $vendor['ven_name'] . 
+                    echo "<tr><th>" . $vendor['ven_num'] . 
+                        "</th><th>" . $vendor['ven_name'] . 
                         "</th><th>" . $vendor['ven_phone'] . 
                         "</th><th>" . $vendor['ven_notes'] . 
                         "</th></tr>";
