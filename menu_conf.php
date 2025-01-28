36d2f9510c3d8beef1f6276744abd4e89c1b2d1e
C M Pipkin <droidclone@gmail.com>
2025-01-27 23:17:59 -0500

    .

--- menu_conf.php
+++ menu_conf.php
@@ -6,11 +6,11 @@ $badgermct_menu_slug = 'badgermct_menu';
 function badgermct_menu() {
     add_menu_page('badgerMCT Dashboard', 'badgerMCT', 'moderate_comments', 'badgermct_dashboard', 'badgermct_dashboard_page', '', 58);
     add_submenu_page('badgermct_dashboard', 'Dashboard', 'Dashboard', 'moderate_comments', 'badgermct_dashboard', 'badgermct_dashboard_page', 1);
-    add_submenu_page($badgermct_menu_slug, 'Manage Culture', 'Culture', 'moderate_comments', 'badgermct_culture', 'badgermct_culture_page', 2);
-    add_submenu_page($badgermct_menu_slug, 'Manage Grain', 'Grain', 'moderate_comments', 'badgermct_grain','badgermct_grain_page', 3);
-    add_submenu_page($badgermct_menu_slug, 'Mange Substrate', 'Substrate', 'moderate_comments', 'badgermct_substrate', 'badgermct_substrate_page', 4);
-    add_submenu_page($badgermct_menu_slug, 'Mange Harvest', 'Harvest', 'moderate_comments', 'badgermct_harvest', 'badgermct_harvest_page', 5);
-    add_submenu_page($badgermct_menu_slug, 'Manage Vendors', 'Vendors', 'moderate_comments', 'badgermct_vendors', 'badgermct_vendors_page', 6);
+    add_submenu_page('badgermct_dashboard', 'Manage Culture', 'Culture', 'moderate_comments', 'badgermct_culture', 'badgermct_culture_page', 2);
+    add_submenu_page('badgermct_dashboard', 'Manage Grain', 'Grain', 'moderate_comments', 'badgermct_grain','badgermct_grain_page', 3);
+    add_submenu_page('badgermct_dashboard', 'Mange Substrate', 'Substrate', 'moderate_comments', 'badgermct_substrate', 'badgermct_substrate_page', 4);
+    add_submenu_page('badgermct_dashboard', 'Mange Harvest', 'Harvest', 'moderate_comments', 'badgermct_harvest', 'badgermct_harvest_page', 5);
+    add_submenu_page('badgermct_dashboard', 'Manage Vendors', 'Vendors', 'moderate_comments', 'badgermct_vendors', 'badgermct_vendors_page', 6);
 }
 /*
 function badgermct_dashboard() {
