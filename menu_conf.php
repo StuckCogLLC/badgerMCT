8b33b02fd060619dd110d5f1f8409f67c045b35c
C M Pipkin <droidclone@gmail.com>
2025-01-27 23:16:38 -0500

    .

--- menu_conf.php
+++ menu_conf.php
@@ -6,22 +6,12 @@ $badgermct_menu_slug = 'badgermct_menu';
 function badgermct_menu() {
     add_menu_page('badgerMCT Dashboard', 'badgerMCT', 'moderate_comments', 'badgermct_dashboard', 'badgermct_dashboard_page', '', 58);
     add_submenu_page('badgermct_dashboard', 'Dashboard', 'Dashboard', 'moderate_comments', 'badgermct_dashboard', 'badgermct_dashboard_page', 1);
-}
-/*function badgermct_culture() {
     add_submenu_page($badgermct_menu_slug, 'Manage Culture', 'Culture', 'moderate_comments', 'badgermct_culture', 'badgermct_culture_page', 2);
-}
-function badgermct_grain() {
     add_submenu_page($badgermct_menu_slug, 'Manage Grain', 'Grain', 'moderate_comments', 'badgermct_grain','badgermct_grain_page', 3);
-}
-function badgermct_substrate() {
     add_submenu_page($badgermct_menu_slug, 'Mange Substrate', 'Substrate', 'moderate_comments', 'badgermct_substrate', 'badgermct_substrate_page', 4);
-}
-function badgermct_harvest() {
     add_submenu_page($badgermct_menu_slug, 'Mange Harvest', 'Harvest', 'moderate_comments', 'badgermct_harvest', 'badgermct_harvest_page', 5);
-}
-function badgermct_vendors() {
     add_submenu_page($badgermct_menu_slug, 'Manage Vendors', 'Vendors', 'moderate_comments', 'badgermct_vendors', 'badgermct_vendors_page', 6);
-}*/
+}
 /*
 function badgermct_dashboard() {
     add_menu_page(
@@ -34,7 +24,7 @@ function badgermct_dashboard() {
         //plugin_dir_url(__FILE__) . 'images/icon_wporg.png',
         81
     );
-}*/
+}
 function badgermct_culture() {
     add_submenu_page(
         'badgermct_dashboard',
@@ -90,7 +80,7 @@ function badgermct_vendors() {
         6
     );
 }
-
+*/
 // menu pages
 function badgermct_dashboard_page() {
     include 'dashboard.php';
