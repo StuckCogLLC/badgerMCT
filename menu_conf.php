fc3f8cddb7a65fecf6f5aa1da2a98f333e630972
C M Pipkin <droidclone@gmail.com>
2025-01-27 23:32:01 -0500

    added comments where function names were not verbose.

--- menu_conf.php
+++ menu_conf.php
@@ -1,12 +1,19 @@
 <?php
 // create menu
 function badgermct_menu() {
+    // main menu title
     add_menu_page('badgerMCT Dashboard', 'badgerMCT', 'moderate_comments', 'badgermct_dashboard', 'badgermct_dashboard_page', '', 58);
+    // dashboard page - same slug as main menu for name overide
     add_submenu_page('badgermct_dashboard', 'Dashboard', 'Dashboard', 'moderate_comments', 'badgermct_dashboard', 'badgermct_dashboard_page', 1);
+    // culture page
     add_submenu_page('badgermct_dashboard', 'Manage Culture', 'Culture', 'moderate_comments', 'badgermct_culture', 'badgermct_culture_page', 2);
+    // grain page
     add_submenu_page('badgermct_dashboard', 'Manage Grain', 'Grain', 'moderate_comments', 'badgermct_grain','badgermct_grain_page', 3);
+    // substrate page
     add_submenu_page('badgermct_dashboard', 'Mange Substrate', 'Substrate', 'moderate_comments', 'badgermct_substrate', 'badgermct_substrate_page', 4);
+    // harvest page
     add_submenu_page('badgermct_dashboard', 'Mange Harvest', 'Harvest', 'moderate_comments', 'badgermct_harvest', 'badgermct_harvest_page', 5);
+    // vendor page
     add_submenu_page('badgermct_dashboard', 'Manage Vendors', 'Vendors', 'moderate_comments', 'badgermct_vendors', 'badgermct_vendors_page', 6);
 }
 
