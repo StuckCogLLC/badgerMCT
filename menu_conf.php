490834a919e7a7a832191e5196023ab9ec588476
C M Pipkin <droidclone@gmail.com>
2025-01-27 22:27:49 -0500

    .

--- menu_conf.php
+++ menu_conf.php
@@ -7,7 +7,7 @@ function badgermct_menu() {
     add_menu_page('badgerMCT', 'badgerMCT', 'moderate_comments', $badgermct_menu_slug, 'badgermct_dashboard_page', '', '', 81);
 }
 function badgermct_submenu() { 
-    //add_submenu_page($badgermct_menu_slug, 'Dashboard', 'Dashboard', 'moderate_comments', 'badgermct_dashboard', 'badgermct_dashboard_page', 1);
+    add_submenu_page($badgermct_menu_slug, 'Dashboard', 'Dashboard', 'moderate_comments', $badgermct_menu_slug, 'badgermct_dashboard_page', 1);
     add_submenu_page($badgermct_menu_slug, 'Manage Culture', 'Culture', 'moderate_comments', 'badgermct_culture', 'badgermct_culture_page', 2);
     add_submenu_page($badgermct_menu_slug, 'Manage Grain', 'Grain', 'moderate_comments', 'badgermct_grain','badgermct_grain_page', 3);
     add_submenu_page($badgermct_menu_slug, 'Mange Substrate', 'Substrate', 'moderate_comments', 'badgermct_substrate', 'badgermct_substrate_page', 4);
