c5ea2e5578c36b7e8f8329d9b7bfb15305d4395d
C M Pipkin <droidclone@gmail.com>
2025-03-03 21:29:11 -0500

    changed cultivation data tables to match outline

--- badgerMCT.php
+++ badgerMCT.php
@@ -14,82 +14,92 @@ global $wpdb;
 if ( ! defined( 'ABSPATH') ) {
     exit;
 }
-
+/* create plugin tables */
 function create_tables() {
     global $wpdb;
     $charset_collate = $wpdb->get_charset_collate();
 
-    $table_badgerMCT_cultures = $wpdb->prefix . 'badgerMCT_cultures';
-    $table_badgerMCT_grain = $wpdb->prefix . 'badgerMCT_grain';
+    /* build table names */
+    /* culture bank */
+    $table_badgerMCT_culture_bank = $wpdb->prefix . 'badgerMCT_culture_bank';
+    /* inoculated cultures */
+    $table_badgerMCT_inoc_culture = $wpdb->prefix . 'badgerMCT_grain';
+    /* inoculated substrate */
     $table_badgerMCT_substrate = $wpdb->prefix . 'badgerMCT_substrate';
+    /* harvested mushrooms */
     $table_badgerMCT_harvest = $wpdb->prefix . 'badgerMCT_harvest';
-    $table_badgerMCT_vendors = $wpdb->prefix . 'badgerMCT_vendors';
 
-    $tbl_badgerMCT_culture = "CREATE TABLE IF NOT EXISTS {$table_badgerMCT_cultures} (
+    /* define tables */
+    /* culture bank */
+    $tbl_badgerMCT_culture_bank = "CREATE TABLE IF NOT EXISTS {$table_badgerMCT_culture_bank} (
+        /* culture number */
         cult_num INT(6) AUTO_INCREMENT,
+        /* culture type */
         cult_type CHAR(50),
+        /* mushroom type */
         mush_type CHAR(50),
+        /* date added */
         date_added DATE,
-        vendor CHAR(50),
-        ven_lot_num CHAR(20),
+        /* origin of culture */
+        origin CHAR(50),
+        /* lot number of origin cluture */
+        origin_lot_number CHAR(20),
+        /* cult_num is primary key */
         PRIMARY KEY(cult_num)
     ) $charset_collate;";
-    
-    $tbl_badgerMCT_grain = "CREATE TABLE IF NOT EXISTS {$table_badgerMCT_grain} (
-        grain_num INT(10) AUTO_INCREMENT,
-        grain_type CHAR(20),
+    /* inoculated cultures */
+    $tbl_badgerMCT_inoc_culture = "CREATE TABLE IF NOT EXISTS {$table_badgerMCT_inoc_culture} (
+        /* inoculation culture number */
+        inoc_cult_num INT(10) AUTO_INCREMENT,
+        /* type of medium used for culture */
+        medium_type CHAR(20),
+        /* from which culture was sample taken */
         cult_num INT(6),
+        /* date of inoculation */
         inoc_date DATE,
-        PRIMARY KEY(grain_num)
+        /* inoc_cult_num is primary key */
+        PRIMARY KEY(inoc_cult_num)
     ) $charset_collate;";
-
+    /* inoculated substrate */
     $tbl_badgerMCT_substrate = "CREATE TABLE IF NOT EXISTS {$table_badgerMCT_substrate} (
+        /* substrate number */
         sub_num INT(6) AUTO_INCREMENT,
-#culture number
-        cult_num INT(6),
-#mushroom type
-        mush_type CHAR(50),
-#mushroom type
-        grain_num INT(10),
-#inoculation data
+        /* culture number */
+        inoc_cult_num INT(6),
+        /* inoculation date */
         inoc_date DATE,
-#fruiting date
+        /* start fruiting date */
         fruit_date DATE,
-#first fruit date
-        first_fruit_weight INT(10),
-#second fruit date
-        second_fruit_weight INT(10),
-#third fruit weight
-        third_fruit_weight INT(10),
-#block weight
+        /* block weight */
         block_weight int(4),
+        /* sub_num is primary key */
         PRIMARY KEY(sub_num)
     ) $charset_collate;";
-
+    /* harvested mushrooms */
     $tbl_badgerMCT_harvest = "CREATE TABLE IF NOT EXISTS {$table_badgerMCT_harvest} (
+        /* harvest number */
         harvest_num INT(10) AUTO_INCREMENT,
+        /* substrate number */
         sub_num INT(6),
+        /* harvest date */
         harvest_date DATE,
+        /* total weight */
         harvest_weight FLOAT(4,2),
+        /* waste weight */
         waste_weight FLOAT(4,2),
+        /* harvest_num */
         PRIMARY KEY(harvest_num)
     ) $charset_collate;";
-
-    $tbl_badgerMCT_vendors = "CREATE TABLE IF NOT EXISTS {$table_badgerMCT_vendors} (
-        ven_num INT(10) AUTO_INCREMENT,
-        ven_name CHAR(50),
-        ven_phone CHAR(11),
-        ven_notes CHAR(250),
-        PRIMARY KEY(ven_num)
-    ) $charset_collate;";
-
     
     require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-    dbDelta( $tbl_badgerMCT_culture );
-    dbDelta( $tbl_badgerMCT_grain );
+    /* culture bank */
+    dbDelta( $tbl_badgerMCT_culture_bank );
+    /* incoulated cultures */
+    dbDelta( $tbl_badgerMCT_inoc_culture );
+    /* inoculated substrate */
     dbDelta( $tbl_badgerMCT_substrate );
+    /* harvested mushrooms */
     dbDelta( $tbl_badgerMCT_harvest );
-    dbDelta( $tbl_badgerMCT_vendors );
 }
 register_activation_hook( __FILE__, 'create_tables' );
 
